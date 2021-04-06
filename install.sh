6fedb4e860bac88622e70808910da14e5d13c19d
Ashton Kinslow <github@ashtonkinslow.com>
2021-04-05 23:26:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -229,6 +229,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+cd $ZSH_CUSTOM/plugins/fast-alias-tips
+make 
+
 
 # TODO:s
 # * finder sidebar
