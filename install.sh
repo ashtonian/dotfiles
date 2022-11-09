639ef45a5b7ded1880fc21435076ffa2c5be7bb7
Ashton Kinslow <github@ashtonkinslow.com>
2022-11-09 00:25:37 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,24 +5,7 @@
 #######################################################################################################################################
 
 eval "$(curl -s -L https://raw.githubusercontent.com/Ashtonian/dotfiles/master/lib_sh/echos.sh)"
-
-# Colors
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
-
-function ok() {
-    echo -e "$COL_GREEN[ok]$COL_RESET "$1
-}
-
-function bot() {
-    echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
-}
+bot "Loaded pretty colors."
 
 #######################################################################################################################################
 # Sudo
@@ -79,6 +62,8 @@ fi;ok
 # ###########################################################
 # install homebrew
 # ###########################################################
+
+export PATH="$PATH:/opt/homebrew/bin"
 running "checking homebrew..."
 brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
@@ -88,7 +73,6 @@ if [[ $? != 0 ]]; then
     error "unable to install homebrew, script $0 abort!"
     exit 2
   fi
-  export PATH="$PATH:/opt/homebrew/bin"
   brew analytics off
 else
   ok
