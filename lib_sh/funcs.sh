a6e6097d3d71acca6e6e0895f147cb9c35766325
Ashton Kinslow <github@ashtonkinslow.com>
2023-04-19 12:56:20 -0400

    update to include casks and seperate out mas, default output to stream with optional file path args

--- lib_sh/funcs.sh
+++ lib_sh/funcs.sh
@@ -193,41 +193,97 @@ function open_chrome_extensions() {
     xdg-open $link || open $link || start chrome $link
   done
 }
-function brew_mas_list() {
-  output_file="software_list.md"
-  directly_installed=false
+
+
+function brew_list_md() {
+  output_stream=true
+  output_file="brew.md"
+  show_dependencies=false
+  inline_dependencies=false
+  include_casks=false
 
   while (( $# > 0 )); do
     case "$1" in
-      -d)
-        directly_installed=true
+      -d|--dependencies)
+        show_dependencies=true
         shift
         ;;
+      -i|--inline-dependencies)
+        inline_dependencies=true
+        shift
+        ;;
+      -c|--casks)
+        include_casks=true
+        shift
+        ;;
+      -o)
+        if [[ -n "$2" && $2 != -* ]]; then
+          output_file="$2"
+          shift 2
+        else
+          shift
+        fi
+        ;;
       -h|--help)
-        echo "Usage: brew_mas_list [-d] [output_file]"
-        echo "Generate a markdown list of software installed via Homebrew and MAS (Mac App Store)."
-        echo "The output file is optional; by default, it is named 'software_list.md'."
-        echo "Use the -d flag to list only directly installed Homebrew formulae."
+        echo "Usage: brew_list_md [-d|--dependencies] [-i|--inline-dependencies] [-c|--casks] [-o output_file]"
+        echo "Generate a markdown list of software installed via Homebrew."
+        echo "Use the -c or --casks flag to lists Homebrew casks in a seperate section."
+        echo "Use the -d or --dependencies flag to list Homebrew dependencies in a separate section."
+        echo "Use the -i or --inline-dependencies flag to list and link dependencies for each Homebrew formula or cask."
+        echo "By default, the output is sent to the standard output stream. Use the -o flag to output to a file (default: brew.md ), optionally specify the filename."
         return 0
         ;;
       *)
-        output_file="$1"
-        shift
+        echo "Unknown argument: $1"
+        echo "Use -h or --help for usage information."
+        return 1
         ;;
     esac
   done
 
-  echo "# Brew and MAS installed software list" > "$output_file"
-  echo "" >> "$output_file"
-
-  echo "## Homebrew" >> "$output_file"
-  echo "" >> "$output_file"
-
-  if $directly_installed; then
-    formulae=$(brew leaves)
-  else
-    formulae=$(brew list --formula -1)
+  if ! $output_stream; then
+    exec 3>&1
+    exec 1> "$output_file"
   fi
+
+    if $include_casks; then
+    echo "## Homebrew Casks"
+    echo ""
+
+    cask_list=$(brew list --cask)
+
+    for cask in ${(f)cask_list}; do
+      info=$(brew info --cask --json=v2 $cask | awk '{gsub(/[[:cntrl:]]/, ""); print}' | tr -d '\r')
+      name=$(echo $info | jq -r '.casks[0].name[0]')
+      homepage=$(echo $info | jq -r '.casks[0].homepage')
+      desc=$(echo $info | jq -r '.casks[0].desc')
+      tap=$(echo $info | jq -r '.casks[0].tap')
+      tap_url="https://github.com/$tap"
+
+      if [[ $tap == "homebrew/cask" ]]; then
+        echo "- [$name]($homepage) - $cask: $desc"
+      else
+        echo "- [$name]($homepage) - $cask ([Tap: $tap]($tap_url)): $desc"
+      fi
+
+      if $inline_dependencies; then
+        dependencies=$(echo $info | jq -r '.casks[0].depends_on.formula[]' 2>/dev/null)
+        if [[ -n $dependencies ]]; then
+          echo "  - Dependencies:"
+          for dep in ${(f)dependencies}; do
+            dep_homepage=$(brew info --json=v2 $dep | jq -r '.formulae[0].homepage')
+            dep_url="https://formulae.brew.sh/formula/$dep"
+            echo "    - [$dep]($dep_homepage) - [$dep]($dep_url)"
+          done
+        fi
+      fi
+    done
+  fi
+
+
+
+
+  formulae=$(brew leaves)
 
   for formula in ${(f)formulae}; do
     name=$(brew info --json=v2 $formula | jq -r '.formulae[0].name')
@@ -235,18 +291,84 @@ function brew_mas_list() {
     desc=$(brew info --json=v2 $formula | jq -r '.formulae[0].desc')
     brew_url="https://formulae.brew.sh/formula/$formula"
 
-    echo "- [$name]($homepage) - [$formula]($brew_url): $desc" >> "$output_file"
+    echo "- [$name]($homepage) - [$formula]($brew_url): $desc"
+
+    if $inline_dependencies; then
+      dependencies=$(brew deps $formula)
+      if [[ -n $dependencies ]]; then
+        echo "  - Dependencies:"
+        for dep in ${(f)dependencies}; do
+          dep_homepage=$(brew info --json=v2 $dep | jq -r '.formulae[0].homepage')
+          dep_url="https://formulae.brew.sh/formula/$dep"
+          echo "    - [$dep]($dep_homepage) - [$dep]($dep_url)"
+        done
+      fi
+    fi
   done
 
-  echo "" >> "$output_file"
-  echo "## Mac App Store" >> "$output_file"
-  echo "" >> "$output_file"
+  if $show_dependencies; then
+    echo ""
+    echo "## Homebrew Dependencies"
+    echo ""
 
-  mas list | while read -r line; do
+    installed_formulae=$(brew list --formula -1)
+    for formula in ${(f)installed_formulae}; do
+      if [[ $(brew uses --installed $formula) ]]; then
+        name=$(brew info --json=v2 $formula | jq -r '.formulae[0].name')
+        homepage=$(brew info --json=v2 $formula | jq -r '.formulae[0].homepage')
+        desc=$(brew info --json=v2 $formula | jq -r '.formulae[0].desc')
+        brew_url="https://formulae.brew.sh/formula/$formula"
+
+        echo "- [$name]($homepage) - [$formula]($brew_url): $desc"
+      fi
+    done
+  fi
+}
+
+function mas_list() {
+  output_stream=true
+  output_file=""
+
+  while (( $# > 0 )); do
+    case "$1" in
+      -o)
+        output_stream=false
+        output_file="$2"
+        shift 2
+        ;;
+      -h|--help)
+        echo "Usage: mas_list [-o output_file]"
+        echo "Generate a markdown list of software installed via MAS (Mac App Store)."
+        echo "By default, the output is sent to the standard output stream. Use the -o flag followed by the output file name to save the output to a file instead."
+        return 0
+        ;;
+      *)
+        echo "Unknown argument: $1"
+        echo "Use -h or --help for usage information."
+        return 1
+        ;;
+    esac
+  done
+
+  if ! $output_stream; then
+    exec 3>&1
+    exec 1> "$output_file"
+  fi
+
+  echo "# Mac App Store installed software list"
+  echo ""
+
+  app_list=$(mas list)
+  for line in ${(f)app_list}; do
     app_id=$(echo $line | awk '{print $1}')
     app_name=$(echo $line | cut -f 2- -d ' ')
     app_store_url="https://apps.apple.com/app/id$app_id"
 
-    echo "- $app_name - [$app_id]($app_store_url)" >> "$output_file"
+    echo "- $app_name - [$app_id]($app_store_url)"
   done
+
+  if ! $output_stream; then
+    exec 1>&3
+  fi
 }
+
